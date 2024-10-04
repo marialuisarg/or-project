1eadf7bc9912d819a375c988d6377f24e806e598
Cecília Romão <ceciliaromao@gmail.com>
2024-10-03 23:14:47 -0300

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,60 +1,53 @@
 import pyomo.environ as pyEnv
 
 def obj_func(model):
-    return sum(model.x[i,j] * model.c[i,j] for i in model.N for j in model.M)
+    return sum(model.x[i, j] * model.c[i, j] for i in model.N for j in model.N)
 
-def rule_const1(model, M):
-    return sum(model.x[i, M] for i in model.N if i != M) == 1
+def rule_const1(model, j):
+    return sum(model.x[i, j] for i in model.N if i != j) == 1
 
-def rule_const2(model, N):
-    return sum(model.x[N, j] for j in model.M if j != N) == 1
+def rule_const2(model, i):
+    return sum(model.x[i, j] for j in model.N if i != j) == 1
 
 def rule_const3(model, i, j):
     n = len(model.N)  # Tamanho do conjunto de nós (número de cidades)
-    if i != j:
-        return model.u[i] - model.u[j] + model.x[i,j] * n <= n-1
+    if i != j and i > 1 and j > 1:
+        return model.u[i] - model.u[j] + model.x[i, j] * n <= n - 1
     else:
-        return model.u[i] - model.u[i] == 0
+        return pyEnv.Constraint.Skip
 
 def create_model(n, dist_matrix):
-    #Model
+    # Model
     model = pyEnv.ConcreteModel()
 
-    #Indexes for the cities
-    model.M = pyEnv.RangeSet(n)                
+    # Indexes for the cities
     model.N = pyEnv.RangeSet(n)
 
-    #Index for the dummy variable u
-    model.U = pyEnv.RangeSet(2,n)
-    
-    #Decision variables xij
-    model.x = pyEnv.Var(model.N,model.M, within=pyEnv.Binary)
+    # Dummy variable u for subtour elimination (starts from city 2)
+    model.u = pyEnv.Var(model.N, within=pyEnv.NonNegativeIntegers, bounds=(0, n - 1))
 
-    #Dummy variable ui
-    model.u = pyEnv.Var(model.N, within=pyEnv.NonNegativeIntegers,bounds=(0,n-1))
-    
-    #Distance Matrix cij
-    model.c = pyEnv.Param(model.N, model.M, initialize=lambda model, i, j: dist_matrix[i-1][j-1])
-    
-    model.objective = pyEnv.Objective(rule=obj_func,sense=pyEnv.minimize)
+    # Decision variables xij
+    model.x = pyEnv.Var(model.N, model.N, within=pyEnv.Binary)
 
-    model.const1 = pyEnv.Constraint(model.M,rule=rule_const1)
-    
-    model.rest2 = pyEnv.Constraint(model.N,rule=rule_const2)
+    # Distance Matrix cij
+    model.c = pyEnv.Param(model.N, model.N, initialize=lambda model, i, j: dist_matrix[i - 1][j - 1])
 
-    # Define the rest3 constraint correctly
+    # Objective Function
+    model.objective = pyEnv.Objective(rule=obj_func, sense=pyEnv.minimize)
+
+    # Constraints
+    model.const1 = pyEnv.Constraint(model.N, rule=rule_const1)
+    model.const2 = pyEnv.Constraint(model.N, rule=rule_const2)
     model.rest3 = pyEnv.Constraint(model.N, model.N, rule=rule_const3)
-    
-    model.pprint()
-    
-    #Solves
-    solver = pyEnv.SolverFactory('glpk')
-    result = solver.solve(model,tee = False)
 
-    #Prints the results
+    # Solver
+    solver = pyEnv.SolverFactory('glpk')
+    result = solver.solve(model, tee=False)
+
+    # Output results
     print(result)
-    
+
     l = list(model.x.keys())
     for i in l:
         if model.x[i]() != 0:
-            print(i,'--', model.x[i]())
+            print(i, '--', model.x[i]())
